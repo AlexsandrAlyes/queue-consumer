bb323835d0d5ac17b23ca832f7a927b1ca652da6
AlAles <alexs.al.ye.s@gmail.com>
2022-10-13 14:36:21 +0300

    add base structur

---
+++ src/test/java/liga/consumer/queue/consumer/module/QueueConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package liga.consumer.queue.consumer.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
